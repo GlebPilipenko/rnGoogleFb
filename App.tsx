931eb82835424616054963c330b60147ad39af5a
Gleb Pilipenka <pilipenka.gleb@gmail.com>
2022-04-15 17:06:20 +0300

    Add logic for correct google authorization on ios platform

--- App.tsx
+++ App.tsx
@@ -1,115 +1,73 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * Generated with the TypeScript template
- * https://github.com/react-native-community/react-native-template-typescript
- *
- * @format
- */
-
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import {Button, SafeAreaView, StyleSheet} from 'react-native';
 import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+  GoogleSignin,
+  GoogleSigninButton,
+  User,
+} from '@react-native-google-signin/google-signin';
+import {REACT_APP_IOS_CLIENT_ID} from './envConfig';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-const Section: React.FC<{
-  title: string;
-}> = ({children, title}) => {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-};
+type Nullable<T> = null | T;
 
 const App = () => {
-  const isDarkMode = useColorScheme() === 'dark';
+  const [isAuth, setIsAuth] = useState<boolean>(false);
+  const [userInfo, setUserInfo] = useState<Nullable<User>>(null);
 
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  const handleUserAuthorization = (
+    isAuthValue: boolean,
+    user: Nullable<User>,
+  ) => {
+    setUserInfo(user);
+    setIsAuth(isAuthValue);
   };
 
+  const onSignInPress = async (): Promise<void> => {
+    try {
+      await GoogleSignin.hasPlayServices();
+      const user: User = await GoogleSignin.signIn();
+
+      handleUserAuthorization(true, user);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const onSignOutPress = async (): Promise<void> => {
+    try {
+      await GoogleSignin.signOut();
+
+      handleUserAuthorization(false, null);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    GoogleSignin.configure({iosClientId: REACT_APP_IOS_CLIENT_ID});
+  }, []);
+
+  console.log(userInfo);
+
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
+    <SafeAreaView style={styles.safeArea}>
+      <GoogleSigninButton
+        style={{width: 192, height: 48}}
+        size={GoogleSigninButton.Size.Wide}
+        color={GoogleSigninButton.Color.Dark}
+        onPress={onSignInPress}
+      />
+
+      {isAuth && <Button title="SignOut" onPress={onSignOutPress} />}
     </SafeAreaView>
   );
 };
 
+export default App;
+
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
+  safeArea: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
   },
 });
-
-export default App;
---
+++ envConfig/index.ts
@@ -0,0 +1,3 @@
+import Config from 'react-native-config';
+
+export const {REACT_APP_IOS_CLIENT_ID} = Config;
